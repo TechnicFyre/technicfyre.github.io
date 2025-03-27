4e833d0ee839a0a8c18157206fbf203e5267c71a
Wil <36526831+TechnicFyre@users.noreply.github.com>
2025-03-26 19:54:18 -0500

    Added "For the current move only, show “You are at move #…” instead of a button."

--- src/App.js
+++ src/App.js
@@ -73,7 +73,14 @@ export default function Game() {
 
   const moves = history.map((squares,move) => {
     let description;
-    if (move > 0) {
+    if (move === currentMove) {
+      description = 'You are at move #' + move;
+      return (
+        <li key={move}>
+          <div>{description}</div>
+        </li>
+      )
+    } else if (move > 0) {
       description = 'Go to move #' + move;
     } else {
       description = 'Go to game start';
